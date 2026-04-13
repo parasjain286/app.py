528c0394f43dd0d50700fcdeeab881bf4da0a271
parasjain286 <141493394+parasjain286@users.noreply.github.com>
2026-04-14 01:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, jsonify
 import requests
 import random
 import re
 import json
 import os
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 API_KEY = "paras286"
 
